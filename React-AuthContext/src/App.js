4df26fa3a2caca2da5ab7f0ca29665086a39254c
Pragati946 <pragatitiwari946@gmail.com>
2022-06-04 15:26:39 +0530

    login

---
+++ React-AuthContext/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Navbar from './component/Navbar';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+     {/* <Navbar/> */}
+
+  
+     <Navbar/>
+    
+    </div>
+  );
+}
+
+export default App;
